8ff86d4faa886ea8d0575821d7d7b87fcd430e27
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2024-09-28 01:40:00 -0500

    PDF || Descargable

--- src/config/global.js
+++ src/config/global.js
@@ -111,7 +111,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/33110014_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
@@ -295,6 +295,16 @@ export default {
         {
           nombre: 'Luis Jesús Pérez Madariaga ',
           cargo: 'Desarrollador <i>full stack</i>',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        },
+        {
+          nombre: 'Daniela Muñoz Bedoya',
+          cargo: 'Animador y productor multimedia',
+          centro: 'Centro de Servicios de Salud - Regional Antioquia',
+        },
+        {
+          nombre: 'Andrés Felipe Guevara Ariza',
+          cargo: 'Locución',
           centro: 'Centro de Servicios de Salud - Regional Antioquia',
         },
       ],
